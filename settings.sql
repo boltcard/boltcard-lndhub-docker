c75be552867ade7fa26d2c16212101a083935c07
Chloe Jung <chloe@onesandzeros.nz>
2023-03-01 11:28:54 +1300

    Update settings.sql

--- settings.sql
+++ settings.sql
@@ -22,5 +22,5 @@ INSERT INTO settings (name, value) VALUES ('AWS_SES_SECRET', '');
 INSERT INTO settings (name, value) VALUES ('AWS_SES_EMAIL_FROM', '');
 INSERT INTO settings (name, value) VALUES ('EMAIL_MAX_TXS', '');
 INSERT INTO settings (name, value) VALUES ('FUNCTION_LNDHUB', 'ENABLE');
-INSERT INTO settings (name, value) VALUES ('LNDHUB_URL', 'http://bolcard_hub:8080');
+INSERT INTO settings (name, value) VALUES ('LNDHUB_URL', 'http://bolcard_hub:9002');
 INSERT INTO settings (name, value) VALUES ('FUNCTION_INTERNAL_API', 'ENABLE');
